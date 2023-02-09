f4820c803aa772d3440d937fe73347103fba12ed
Ramastix <ramastix@gmail.com>
2023-02-09 09:38:30 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 06;
+        post.birthday.year = 1999;
+        post.name = "Ivan";
+        post.patronymic = "Ivanovich";
+        post.surname = "Ivanov";
+        post.passport = "4444 № 44444444";
+        post.phone = "+7(999)-999-99-99";
+        post.subscription = true;
+    }
+}
